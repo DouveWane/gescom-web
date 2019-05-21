66b771e04ca893cad64da91b43756892478091e0
Birane Baila <douvewane85@gmail.com>
2019-05-21 11:12:03 +0000

    Suppression component Home et Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './route/app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './home/navbar/navbar.component';
+
+import * as $ from 'jquery';
+window['$'] = window['jQuery'] = $;
 import { APP_BASE_HREF } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
